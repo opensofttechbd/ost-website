84bfefda4ae480dc6ea3046f0992c9d13103eb2d
sarjis <sarjis.m.a@gmail.com>
2023-02-07 18:55:41 +0600

    tailwind added

--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,13 @@
 import Default from '@/layout/Default'
 
-
 export default function Contact() {
   return (
     <>
-      <Default>contact page</Default>
+      <Default>contact page
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+      </Default>
     </>
   )
 }
